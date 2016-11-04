a0b1d02d154a6517027d8ed8afe90cab8718705e
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2016-11-04 16:37:43 +0000

    working bing clone

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app = express();
+var Search = require('bing.search');
+var key = require('./key');
+var search = new Search(key.mc());
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+//console.log(key.mc());
+app.get('/:search', function (req, res) {
+    var search_key = req.params.search;
+    search.images(search_key,
+        {top: 10},
+        function(err, results) {
+            if (err) throw err;
+            res.send((results));
+        }
+    );
+    //res.send(search);
 }).listen(8080);
